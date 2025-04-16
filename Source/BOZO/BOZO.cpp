205e215329d3dbd19765aed1660bc9d4e816ef75
Sachaleplankton <231418@umons.ac.be>
2025-04-16 17:25:44 +0200

    Revert "C++"
    This reverts commit 86a5eaebd682f08548799f39af43c395f4562476.

--- Source/BOZO/BOZO.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BOZO.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BOZO, "BOZO" );
